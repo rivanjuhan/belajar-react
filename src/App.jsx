d3590c2597f45a076a9f9d9b1945b3b3e73eedfb
rivanjuhan <rivannila26@gmail.com>
2025-01-26 21:17:18 +0700

    test change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Belajar Vite + React</h1>
+      <h1>Mari belajar React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Nilai {count}
